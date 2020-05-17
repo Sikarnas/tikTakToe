ca2e59d20c337acdef685b9dbb61234455ea3a5a
Aurelijus Zinis <aurelijuszinis@aurelijus-z-vilm.local>
2020-05-17 10:47:51 +0300

    fixed frontend folder spelling error

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './fronend/tikTakToe.js',
+  entry: './frontend/tikTakToe.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
